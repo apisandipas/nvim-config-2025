04340ef7113835a376b615f14f78a058d0b1545a
Bryan Paronto <bryan.paronto@atomicobject.com>
2025-03-31 07:56:27 -0500

    removed unneed deps from neogit

--- lua/bryan/neogit.lua
+++ lua/bryan/neogit.lua
@@ -3,11 +3,7 @@ local M = {
   dependencies = {
     "nvim-lua/plenary.nvim", -- required
     "sindrets/diffview.nvim", -- optional - Diff integration
-
-    -- Only one of these is needed.
     "nvim-telescope/telescope.nvim", -- optional
-    "ibhagwan/fzf-lua", -- optional
-    "echasnovski/mini.pick", -- optional
   },
   config = true,
 }
